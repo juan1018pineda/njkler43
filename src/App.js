086c73ef315e99caef16e18dd55ffdb236c6dcd1
pineda.juan92 <pineda.juan92@gmail.com>
2022-01-05 19:54:26 -0500

    State and function created to capture input and filter posts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import posts from './posts'
-
+import React, { Component } from "react";
+import posts from "./posts";
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super();
+    this.handleQuery = this.handleQuery.bind(this);
+    this.filteredPosts = posts;
+
+    this.state = {
+      query: "",
+    };
+  }
+  handleQuery(event) {
+    this.setState({
+      query: event.target.value.toLowerCase(),
+    });
+    this.search();
+  }
+
+  search() {
+    const query = this.state.query;
+    this.filteredPosts = posts.filter(function (post) {
+      return post.title.toLowerCase().match(query);
+    });
+    console.log(this.state.query, this.filteredPosts);
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input
+            type="text"
+            placeholder="Ingresa el término de búsqueda"
+            value={this.state.query}
+            onChange={this.handleQuery}
+          />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.filteredPosts.map((post, key) => (
+            <li key={key}>
+              <a href={post.url}>
+                <img src={post.image} alt={post.title} />
+              </a>
+              <p>{post.title}</p>
+            </li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-
-export default App
-
-
+export default App;
